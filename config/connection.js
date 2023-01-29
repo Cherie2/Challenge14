5d08899b00cf721b5f1ec3749174e728c49f3eb6
Cherie2 <cwalk280@yahoo.com>
2023-01-29 01:16:16 -0800

    revised connection with JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 
 let sequelize;
 
+
+if (process.env.JAWSDB_URL){
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else{(
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -11,7 +15,7 @@ let sequelize;
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    }
-  );
+    })
+  )};
 
 module.exports = sequelize;
